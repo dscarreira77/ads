26ddc1c598465573ffb4c0e51b23fdef8629ef8f
fc58629 <fc58629@alunos.fc.ul.pt>
2024-05-27 19:55:20 +0100

    src
    dsa

---
+++ src/domain/Contexto.java
@@ -0,0 +1,26 @@
+package domain;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Contexto {
+	
+	private String designacao;
+	private Map<String,CaracteristicaContexto> carcons;
+    // ...
+
+	public Contexto(String desig) {
+		carcons = new HashMap<>();
+		designacao = desig;
+		// ...
+	}
+
+	public void adicionaCaracteristica(CaracteristicaContexto carCont) {
+		carcons.put(carCont.nomeCaracteristica(), carCont);
+		carCont.addObserver(this);
+	}
+
+	public String obtemDesignacao() {
+		return designacao;
+	}
+}
---
+++ src/domain/GoodPlaces.java
@@ -0,0 +1,48 @@
+package domain;
+
+
+import domain.handlers.CriarCaracteristicaHandler;
+import domain.handlers.LoginHandler;
+import domain.handlers.LogoutHandler;
+import domain.handlers.RecolherDadosHandler;
+import domain.interfaces.ICriarCaracteristicaHandler;
+import domain.interfaces.IGoodPlaces;
+import domain.interfaces.ILoginHandler;
+import domain.interfaces.ILogoutHandler;
+import domain.interfaces.IRecolherDadosHandler;
+import services.SessionManager;
+
+/**
+ * Classe que define o objeto inicial da aplicacao
+ * 
+ * @author ADS
+ */
+public class GoodPlaces implements IGoodPlaces {
+	private CatalogoDescricaoCaracteristicas catDescrCaract;
+	private CatalogoContextos catContexts;
+	private CatalogoUtilizadores catUser;
+	private CatalogoTiposSensor catTiposSensor;
+	private CatalogoUnidades catUnidades;
+
+    /**
+     * Construtor
+     */
+	public GoodPlaces() {
+    	catUser = new CatalogoUtilizadores();       // Ja' sao criados alguns utilizadore
+    	catUnidades = new CatalogoUnidades();       // Ja' sao criadas algumas unidades
+    	catTiposSensor = new CatalogoTiposSensor();
+    	catDescrCaract = new CatalogoDescricaoCaracteristicas();
+    	catContexts = new CatalogoContextos();
+    	// Criar alguns objetos necessarios para testar os casos de uso UC6
+    	// e UC11 porque os outros casos de uso não estao ainda implementados:
+    	catTiposSensor.loadSomeTipoSensor(catUnidades); // Cria 4 Tipos de sensor e sensores associados
+    	catDescrCaract.loadSomeDescCar(catTiposSensor, catUnidades);  // Cria 2 caracteristicas
+    	catContexts.loadSomeContexts(catDescrCaract, catUser);   // Cria dois contextos
+    }
+    
+	@Override
+	public String obtemTipoUserAutenticado() {
+		return getAuthenticatedUser().getClass().getName().substring(7);
+	}
+
+}
